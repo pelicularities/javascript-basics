657e0a88ba02280792ea528186c6191ddcd58ed9
Grace Teng <pelicularities@users.noreply.github.com>
2021-01-20 14:14:11 +0800

    complete 1-functions.js

--- 1-functions.js
+++ 1-functions.js
@@ -11,8 +11,8 @@ assertEquals(hello(), "hello world");
 
 /* ---------------------- EXERCISE 2 ---------------------- */
 // Define a function, greet(personName), that returns 'hello specificPersonName'. If no name is supplied, return 'hello world'
-function greet(name) {
-
+function greet(name = 'world') {
+  return `hello ${name}`;
 }
 
 // Assertions (do not change)
@@ -21,7 +21,9 @@ assertEquals(greet(), "hello world");
 
 /* ---------------------- EXERCISE 3 ---------------------- */
 // Define a function, add2Numbers(num1, num2), to return the sum of 2 values
-
+function add2Numbers(num1, num2) {
+  return num1 + num2;
+}
 
 // Assertions (do not change)
 assertEquals(add2Numbers(1, 1), 2);
@@ -29,7 +31,17 @@ assertEquals(add2Numbers(100, 1), 101);
 
 /* ---------------------- EXERCISE 4 ---------------------- */
 // Define a function, invertCase(someString), that returns the input string with its case inverted
-
+function invertCase(someString) {
+  let invertedString = '';
+  for (char of someString) {
+    if (char.toLowerCase() === char) {
+      invertedString += char.toUpperCase();
+    } else {
+      invertedString += char.toLowerCase();
+    }
+  }
+  return invertedString;
+}
 
 // Assertions (do not change)
 assertEquals(invertCase("Hello"), "hELLO");
